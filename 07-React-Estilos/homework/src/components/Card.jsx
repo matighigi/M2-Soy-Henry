77f853991fbaf81c627b9fbd7e51b24562c189e5
Matias Agustin Ghigi <matiasghigi@gmail.com>
2022-10-04 21:01:20 -0300

    Tarea 07 Realizada

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import style from './Card.module.css'
 
 export default function Card(props) {
-  // acá va tu código
-  return <div>Card Component</div>
+ 
+  return (<div className={style.div}>
+            <button onClick={props.onClose} className={style.boton}>X</button>
+            <div>
+              <h4 className={style.titulo}>{props.name}</h4>
+              <div className={style.info}>
+                <div>
+                  <p className={style.maxmin}>Min</p>
+                  <p className={style.min}>{props.min}</p>
+                </div>
+                <div>
+                  <p className={style.maxmin}>Max</p>
+                  <p className={style.max}>{props.max}</p>
+                </div>
+              </div>
+            </div>
+            <img className={style.img} 
+            src = {`http://openweathermap.org/img/wn/${props.img}@2x.png`} 
+            /> 
+          {/* prestar atencion a ${props.img} que con destructuring sería ${img} */}
+          </div>
+  )
 };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import style from './SearchBar.module.css'
 
 export default function SearchBar(props) {
-  // acá va tu código
-  return <div>Search Bar Component</div>
+  
+  return (
+    <div className={style.container}>
+      
+      <input type={'text'} 
+      placeholder={'Ciudad...'}
+      className={style.barra}>
+      </input>
+      
+
+      
+      <button className = {style.button}
+      onClick={() => props.onSearch('Buscando Ciudad')}>
+        Agregar
+      </button>
+      
+    </div>
+  )
 };
